eb5b9bae5418e9f338846d4fba1ea70cd4f45af7
Mkumar <manyakumar1205@gmail.com>
2026-04-16 22:13:36 +0530

    Add message id

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://ws.ifelse.io');
 
-let waitingtime = false;
-let start;
+let msgid = 0;
+const pending = {};
 
 ws.on('open', () => {
   console.log("Connected to server");
   setInterval(() => {
-    const message = "Hello";
+    msgid++;
+    const message = JSON.stringify({ id : msgid, text: "Hello" });
 
     start = process.hrtime.bigint();
-    waitingtime = true;
+    pending[msgid] = start;
 
     ws.send(message);
   },1000); // send every 1 second 
 });
 
 ws.on('message', (data) => {
-  if (waitingtime && data.toString() === "Hello") {
+  let parsed;
+
+  try { 
+    parsed = JSON.parse(data.toString());
+  }
+  catch {
+    console.log("Ignored non-JSON message:", data.toString());
+    return;
+  }
+
+  const id = parsed.id;
+  if (pending[id]) {
     const end = process.hrtime.bigint();
-    const latency = Number(end - start) / 1e6;
+    const latency = Number(end - pending[id]) / 1e6;
 
-    waitingtime = false;
-
-    console.log("Received:", data.toString());
+    console.log(`Received response for message ID ${id}:`, parsed.text);
     console.log("Latency:", latency.toFixed(2), "ms");
 
     //ws.close(); // close ONLY after correct response
   } else {
-    console.log("Ignored message:", data.toString());
+    console.log("Unknown message:", data.toString());
   }
 });
 
